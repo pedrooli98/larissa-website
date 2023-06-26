3f86a20af8cf8e87755e78bd43cd352172acec48
pedrooli98 <pedroblizz@outlook.com>
2023-06-26 20:11:18 -0300

    feat: create first part of the site body

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,29 @@
 <script>
   import Header from './components/Header.svelte'
+  import Body from './components/Body.svelte'
 </script>
 
-<div>
+<div class="main">
     <Header/>
+    <div class="body">
+      <Body/>
+    </div>
 </div>
+
+<style>
+  .main {
+    /* background-color: red; */
+    width: 100vw;
+    height: 100vh;
+    width: 100%;
+    background-image: url('./img/castelo.png');
+  }
+
+  .body {
+    width: 100vw ;
+    height: 100vh ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ src/components/Body.svelte
@@ -0,0 +1,59 @@
+<script>
+    import Logo from '../img/logo.png'
+</script>
+
+<section>
+    <div>
+        <p>
+            design, conteúdo & filtros para <span class="italic">agências</span> e marcas incríveis.
+        </p>
+        <p>
+            a sua super personalidade com a <span class="bold">força do</span> 🐐 para <span class="italic">uma marca autêntica e destacada no mundo.</span>
+        </p>        
+    </div>
+    
+   <h1>
+        <img src={Logo} alt="website logo">
+    </h1>
+   
+</section>
+
+<style>
+    section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 4rem;
+    }
+
+    section > h1 > img {
+        max-width: 400px;
+        min-width: 150px;
+        width: 100%;
+    }
+
+    section > div {
+        max-width: 500px;
+        max-width: 300px;
+        width: 100%;
+        height: 110px;
+        display: flex;
+        flex-flow: column;
+        justify-content: space-between;
+    }
+
+    section > div > p {
+        font-size: 0.9rem;
+        text-transform: uppercase;
+        color: #DE8D78;
+        line-height: 1.2rem;
+    }
+    section > div > p >.italic {
+        font-weight: 300;
+        font-style: italic;   
+    }
+    section > div > p > .bold {
+        font-weight: 600;
+    }
+</style>
+
